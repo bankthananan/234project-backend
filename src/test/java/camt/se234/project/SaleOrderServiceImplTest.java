d5c23b21c24859ea7d2735f989adce22fcd91efa
Unknown <thananan157@hotmail.com>
2018-05-08 21:58:20 +0700

    Test get sale order
    create getSaleOrders mothod

--- src/test/java/camt/se234/project/SaleOrderServiceImplTest.java
+++ src/test/java/camt/se234/project/SaleOrderServiceImplTest.java
@@ -1,11 +1,21 @@
 package camt.se234.project;
 
 import camt.se234.project.dao.OrderDao;
+import camt.se234.project.entity.Product;
+import camt.se234.project.entity.SaleOrder;
+import camt.se234.project.entity.SaleTransaction;
 import camt.se234.project.service.SaleOrderServiceImpl;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SaleOrderServiceImplTest {
     OrderDao orderDao;
@@ -17,5 +27,23 @@ public class SaleOrderServiceImplTest {
         saleOrderService = new SaleOrderServiceImpl();
         saleOrderService.setOrderDao(orderDao);
     }
-   
+    @Test
+    public void getSaleOrders(){
+        List<SaleTransaction> mockTransaction = new ArrayList<>();
+        List<SaleOrder> mockOrder = new ArrayList<>();
+        mockTransaction.add(new SaleTransaction(null,"T0001",new SaleOrder(null,"S0001",mockTransaction),
+                new Product(null,"p0001","test1","test1","test1.jpg",5000),
+                10));
+        mockTransaction.add(new SaleTransaction(null,"T0002",new SaleOrder(null,"S0002",mockTransaction),
+                new Product(null,"p0002","test2","test2","test2.jpg",2000),
+                20));
+        mockOrder.add(new SaleOrder(null,"S0001",mockTransaction));
+        mockOrder.add(new SaleOrder(null,"S0002",mockTransaction));
+
+        when(orderDao.getOrders()).thenReturn(mockOrder);
+
+        assertThat(saleOrderService.getSaleOrders(),hasItems(new SaleOrder(null,"S0001",mockTransaction),
+                new SaleOrder(null,"S0002",mockTransaction)));
+
+    }
 }
