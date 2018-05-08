118d7836d4400d9ea89b96c32d611a8a7b9b6e09
Unknown <thananan157@hotmail.com>
2018-05-08 18:47:02 +0700

    Add test classes

---
+++ src/test/java/camt/se234/project/AuthenticationServiceTest.java
@@ -0,0 +1,17 @@
+package camt.se234.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthenticationServiceTest {
+    @Test
+    public void AuthenticationServiceTest(){
+
+    }
+}
