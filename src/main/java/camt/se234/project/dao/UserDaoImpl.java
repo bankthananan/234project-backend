66992c4a5082798bd8dff7c62a84dc0d14b31e10
Unknown <thananan157@hotmail.com>
2018-05-08 20:44:32 +0700

    Add JUnitParams
    test with JUnitParams

--- src/main/java/camt/se234/project/dao/UserDaoImpl.java
+++ src/main/java/camt/se234/project/dao/UserDaoImpl.java
@@ -13,4 +13,6 @@ public class UserDaoImpl implements UserDao{
     public User getUser(String username, String password) {
         return userRepository.findByUsernameAndPassword(username,password);
     }
+
+
 }
--- src/test/java/camt/se234/project/AuthenticationServiceTest.java
+++ src/test/java/camt/se234/project/AuthenticationServiceTest.java
@@ -1,7 +1,6 @@
 package camt.se234.project;
 
 import camt.se234.project.dao.UserDao;
-import camt.se234.project.dao.UserDaoImpl;
 import camt.se234.project.entity.User;
 import camt.se234.project.service.AuthenticationServiceImpl;
 import org.junit.Before;
@@ -9,18 +8,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(JUnitParamsRunner.class)
 public class AuthenticationServiceTest {
     UserDao userDao;
     AuthenticationServiceImpl authenticationService;
@@ -34,13 +30,14 @@ public class AuthenticationServiceTest {
 
     @Test
     public void AuthenticationServiceTest(){
-
         when(userDao.getUser("admin","admin")).thenReturn(new User(null,"admin","admin","admin"));
         when(userDao.getUser("A","A")).thenReturn(new User(null,"A","A","user"));
         when(userDao.getUser("B","B")).thenReturn(new User(null,"B","B","user"));
+
 
         assertThat(userDao.getUser("admin","admin"),is(new User(null,"admin","admin","admin")));
         assertThat(userDao.getUser("A","A"),is(new User(null,"A","A","user")));
         assertThat(userDao.getUser("B","B"),is(new User(null,"B","B","user")));
     }
+
 }
